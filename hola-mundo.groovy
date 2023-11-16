e9acdd3abf3878843635c3a3bd966c3741978c83
Kells02 <cristianfermar01@gmail.com>
2023-11-16 16:39:35 +0100

    Hola Mundo

--- hola-mundo.groovy
+++ hola-mundo.groovy
@@ -1,9 +1,15 @@
+// Definición del pipeline
 pipeline {
+    // Especifica que el pipeline puede ejecutarse en cualquier agente disponible
     agent any
 
+    // Definición de las etapas del pipeline
     stages {
+        // Etapa llamada "Hello"
         stage('Hello') {
+            // Pasos a ejecutar en la etapa
             steps {
+                // Imprime 'Hello World!' en la consola de salida de Jenkins
                 echo 'Hello World!'
             }
         }
