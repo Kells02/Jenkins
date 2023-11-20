706d30720678d3e50016b910fde4f2e83573b1f2
Cristian Fernández Márquez <101474487+Kells02@users.noreply.github.com>
2023-11-20 11:56:26 +0100

    Add files via upload

---
+++ stage-folders.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+
+def main():
+    #client_folder = "D:\Tenea\Jenkins\Client\Prueba"  # Actualiza con la ruta correcta
+    client_folder = 'Prueba/'
+
+    if os.path.exists(client_folder):
+        print("La carpeta del cliente ya existe. Verificando...")
+        if verify_template(client_folder):
+            print("Carpeta del cliente verificada exitosamente.")
+        else:
+            print("Error en la verificación de la carpeta del cliente. Iniciando rollback...")
+            rollback(client_folder)
+    else:
+        print("La carpeta del cliente no existe. Copiando la plantilla...")
+        # Agrega aquí las acciones necesarias para copiar la plantilla
+        copy_template(client_folder)
+
+        # Verifica la plantilla copiada
+        if verify_template(client_folder):
+            print("Carpeta del cliente creada y verificada exitosamente.")
+        else:
+            print("Error en la verificación de la carpeta del cliente. Iniciando rollback...")
+            rollback(client_folder)
+
+def copy_template(client_folder):
+    # Crea la carpeta del cliente si no existe
+    if not os.path.exists(client_folder):
+        os.makedirs(client_folder)
+
+def verify_template(client_folder):
+    # Agrega aquí las acciones necesarias para verificar la plantilla
+    # En este ejemplo, simplemente verificamos si la carpeta existe
+    return os.path.exists(client_folder)
+
+def rollback(client_folder):
+    # Agrega aquí las acciones necesarias para el rollback
+    print("Eliminando la carpeta del cliente durante el rollback...")
+    shutil.rmtree(client_folder)
+    print("Carpeta del cliente eliminada exitosamente durante el rollback.")
+
+if __name__ == "__main__":
+    main()
